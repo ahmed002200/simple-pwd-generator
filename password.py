14883c7512b2819592af20103270cafc2d83e2a8
Ahmed <ahmed.s.yas6400@gmail.com>
2025-08-07 20:21:58 +0300

    added some proofing

--- password.py
+++ password.py
@@ -15,15 +15,21 @@ def generate(num_of_chars, include_numbers, include_special):
         password[random_index] = password[random_index].upper()
     if include_numbers == "yes":
         for _ in range(0, math.ceil((len(password)-1) * 0.3)):
-            password[random.randint(0, len(password)-1)] = random.randint(0,9)
-    if include_special == "yes":
-        for _ in range(0, math.ceil((len(password)-1) * 0.1)):
             while True:
                 number_index = random.randint(0, len(password)-1)
                 item = str(password[number_index])
                 if item.isalpha():
                     if item.islower():
-                        password[number_index] = random.choice(special_chars)
+                        password[number_index] = random.randint(0,9)
+                        break
+    if include_special == "yes":
+        for _ in range(0, math.ceil((len(password)-1) * 0.1)):
+            while True:
+                special_index = random.randint(0, len(password)-1)
+                item = str(password[special_index])
+                if item.isalpha():
+                    if item.islower():
+                        password[special_index] = random.choice(special_chars)
                         break
 
     result = ''.join(str(item) for item in password)
@@ -39,11 +45,14 @@ def start():
             num_of_chars = int(input("How many characters would you like your password to be (8-32)? "))
             include_numbers = input("Would you like to include numbers in you password (yes/no)? ").lower()
             include_special = input("Would you like to include special characters in your password (yes/no)? ").lower()
-            if (include_numbers in answers) and (include_special in answers):
-                generate(num_of_chars, include_numbers, include_special)
-                break
+            if 8 <= num_of_chars <= 32:
+                if (include_numbers in answers) and (include_special in answers):
+                    generate(num_of_chars, include_numbers, include_special)
+                    break
+                else:
+                    print("Only yes or no allowed. Try again.")
             else:
-                print("Only yes or no allowed. Try again.")
+                print("Please enter a valid range of characters (8-32)")
         except ValueError:
             print("Your input was invalid. Try again.")
 
